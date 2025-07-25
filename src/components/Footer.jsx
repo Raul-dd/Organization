41177675aa22a0eb6afeb82644999d6de288ad7e
Raul Diaz <rauldiaz@micorreo.upp.edu.mx>
2025-07-24 21:56:26 -0600

    FrontEnd Term

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 const Footer = () => {
   return (
     <footer className="bg-[#002843] text-white py-4 text-center px-4">
-      <p className="text-base sm:text-sm md:text-xs lg:text-[0.95rem] max-w-screen-xl mx-auto">
-  Ayudar es Amar AC © Todos los derechos reservados
-</p>
-
+      <p className="text-xs sm:text-sm md:text-base lg:text-[0.95rem] max-w-screen-xl mx-auto">
+      Ayudar es Amar AC © Todos los derechos reservados
+    </p>
     </footer>
   );
 };
